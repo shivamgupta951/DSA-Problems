a15042c71047adbc7f6741aa98a6a023c37a3be5
Shivam Gupta <kapilmohangupta20@gmail.com>
2025-09-27 22:40:19 +0530

    LC -> 189 (Optimal Approach)!

--- prog6.java
+++ prog6.java
@@ -3,10 +3,38 @@ Given an integer array nums, rotate the array to the right by k steps, where k i
  ->3rd Approach! O(n)
 */
 
-public class prog6 {
-    
+class prog6 {
+    public static void RotateArray(int[] nums, int k) {
+        /* Swap whole array~ */
+        for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
 
-    /* array reversal method and then sub reverse by k parts! */
+        /* swap first k elements of the array~ */
+        for (int i = 0, j = k - 1; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
 
-    
+        /* swap last n-k elements of the array~ */
+        for (int i = k, j = nums.length - 1; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int[] array = {1,2,3,4,5,6,7,8};
+        RotateArray(array,3);
+        for(int ele : array)
+        {
+            System.out.print(ele+" ");
+        }
+    }
 }
