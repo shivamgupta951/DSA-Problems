e24fdc35d5272d3bd8ac1578d3c15e0d92616f03
Shivam Gupta <kapilmohangupta20@gmail.com>
2025-10-17 23:40:15 +0530

    LC-> 125 (Optimal Solution!)

---
+++ prog23.java
@@ -0,0 +1,29 @@
+/* 125. Valid Palindrome 
+   A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same 
+   forward and backward. Alphanumeric characters include letters and numbers.
+   1st Approach O(N)!
+*/
+class prog23 {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main (String args[])
+    {
+        /* test case! */
+    }
+}
