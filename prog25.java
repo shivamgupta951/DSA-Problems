8b06480891ce0d9a36b0d4cf01742204d96b935b
Shivam Gupta <kapilmohangupta20@gmail.com>
2025-10-21 12:55:30 +0530

    LC -> 35

---
+++ prog25.java
@@ -0,0 +1,31 @@
+/* 35. Search Insert Position
+   Given a sorted array of distinct integers and a target value,
+   return the index if the target is found. If not, return the index where it would be if it were 
+   inserted in order.You must write an algorithm with O(log n) runtime complexity.
+   -> 1st Attempt (Optimal Solution!)
+*/
+
+class prog25 {
+    public static int searchInsert(int[] nums , int target)
+    {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end)
+        {
+            int mid = start + (end - start)/2;
+            if(nums[mid]>target) end = mid-1;
+            else if (nums[mid]==target) return mid;
+            else start = mid+1;
+        }
+        return start;
+    }
+
+    public static void main(String args[])
+    {
+        int[] array = {1,3,5,6};
+        int target = 7;
+        int result = searchInsert(array,target);
+        System.out.print(result);
+    }
+    
+}
