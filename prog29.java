b0b4acf1ac27313d94e54cc5c612791c09109bf2
Shivam Gupta <kapilmohangupta20@gmail.com>
2025-10-27 23:22:24 +0530

    LC -> 3 (Optimal Solution)!

--- prog29.java
+++ prog29.java
@@ -1,14 +1,13 @@
 import java.util.Stack;
 
-/**
- * 20. Valid Parentheses
- * Given a string s containing just the characters '(', ')', '{', '}', '[' and
- * ']', determine if the input string is valid.
- * An input string is valid if:
- * Open brackets must be closed by the same type of brackets.
- * Open brackets must be closed in the correct order.
- * Every close bracket has a corresponding open bracket of the same type.
- * -> 1st Approach (Optimal)
+/* 20. Valid Parentheses
+  Given a string s containing just the characters '(', ')', '{', '}', '[' and
+  ']', determine if the input string is valid.
+  An input string is valid if:
+  Open brackets must be closed by the same type of brackets.
+  Open brackets must be closed in the correct order.
+  Every close bracket has a corresponding open bracket of the same type.
+  -> 1st Approach (Optimal)
  */
 
 public class prog29 {
--- prog32.java
+++ prog32.java
@@ -3,15 +3,28 @@
    -> 2nd Approach O(N)!
 */
 
+import java.util.HashMap;
+
 public class prog32 {
     public static int lolss(String s) {
         int max = 0;
-        
+        int start = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int end = 0; end<s.length(); end++)
+        {
+            char c = s.charAt(end);
+            if(map.containsKey(c))
+            {
+                start = Math.max(start,map.get(c)+1);
+            }
+            map.put(c,end);
+            if(max<end-start+1) max=end-start+1;
+        }
         return max;
     }
     
     public static void main(String args[])
     {
-        System.out.println(lolss("abcabcbb"));
+        System.out.println(lolss("pwwkew"));
     }
 }
