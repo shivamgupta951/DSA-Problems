99830093678172bb3c9b82f4c27fb71066207926
Shivam Gupta <kapilmohangupta20@gmail.com>
2026-03-13 22:06:29 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 79.66%] - LeetPush

--- Unique-Paths.java
+++ Unique-Paths.java
@@ -1,34 +1,24 @@
 1class Solution {
-2    int[] dx = {0,1};
-3    int[] dy = {1,0};
-4    public int solve(int i , int j , int m , int n , int[][] dp)
-5    {
-6        if(i==m-1 && j==n-1)
-7        {
-8            return 1;
-9        }
-10        if(dp[i][j]!=-1) return dp[i][j];
-11        int ways = 0;
-12        for(int k=0; k<2; k++)
-13        {
-14            int r = i + dx[k];
-15            int c = j + dy[k];
-16            if(r<m && c<n)
-17            {
-18                ways += solve(r,c,m,n,dp);
+2    public int solve(int i, int j, int[][] dp) {
+3        if (i == 0 && j == 0)
+4            return 1;
+5        if (i < 0 || j < 0)
+6            return 0;
+7        if (dp[i][j] != -1)
+8            return dp[i][j];
+9        int up = solve(i - 1, j, dp);
+10        int left = solve(i, j - 1, dp);
+11        return dp[i][j] = up + left;
+12    }
+13
+14    public int uniquePaths(int m, int n) {
+15        int dp[][] = new int[m][n];
+16        for (int i = 0; i < m; i++) {
+17            for (int j = 0; j < n; j++) {
+18                dp[i][j] = -1;
 19            }
 20        }
-21        return dp[i][j] = ways;
+21        return solve(m - 1, n - 1, dp);
 22    }
-23    public int uniquePaths(int m, int n) {
-24        int[][] dp = new int[m][n];
-25        for(int i=0; i<m; i++)
-26        {
-27            for(int j=0; j<n; j++)
-28            {
-29                dp[i][j] = -1;
-30            }
-31        }
-32        return solve(0,0,m,n,dp);
-33    }
-34}
+23}
+24
