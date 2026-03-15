8b1f20a448578c443391a900653f0c3d5437dab8
Shivam Gupta <kapilmohangupta20@gmail.com>
2026-03-15 16:00:31 +0530

    [1463] [Time Beats: 24.19%] [Memory Beats: 85.87%] - LeetPush

--- Cherry-Pickup-II.java
+++ Cherry-Pickup-II.java
@@ -1,41 +1,41 @@
 1class Solution {
-2    public int solve(int i, int j1, int j2, int m, int n, int[][] grid, int[][][] dp) {
-3        if (j1 < 0 || j2 < 0 || j1 > n || j2 > n)
-4            return (int) -1e8;
-5        if (i == m) {
-6            if (j1 == j2)
-7                return grid[i][j1];
-8            else
-9                return grid[i][j1] + grid[i][j2];
-10        }
-11        if (dp[i][j1][j2] != -1)
-12            return dp[i][j1][j2];
-13        int maxi = (int) -1e8;
-14        for (int dj1 = -1; dj1 <= 1; dj1++) {
-15            for (int dj2 = -1; dj2 <= 1; dj2++) {
-16                int value = 0;
-17                if (j1 == j2)
-18                    value = grid[i][j1];
-19                else
-20                    value = grid[i][j1] + grid[i][j2];
-21                value += solve(i + 1, j1 + dj1, j2 + dj2, m, n, grid, dp);
-22                maxi = Math.max(value, maxi);
-23            }
-24        }
-25        return dp[i][j1][j2] = maxi;
-26    }
-27
-28    public int cherryPickup(int[][] grid) {
-29        int m = grid.length;
-30        int n = grid[0].length;
-31        int[][][] dp = new int[m][n][n];
-32        for (int i = 0; i < m; i++) {
-33            for (int j = 0; j < n; j++) {
-34                for (int k = 0; k < n; k++) {
-35                    dp[i][j][k] = -1;
-36                }
-37            }
+2    public int cherryPickup(int[][] grid) {
+3        int m = grid.length;
+4        int n = grid[0].length;
+5        int prev[][] = new int[n][n];
+6        for (int i = 0; i < n; i++) {
+7            for (int j = 0; j < n; j++) {
+8                if (i == j)
+9                    prev[i][j] = grid[m - 1][i];
+10                else
+11                    prev[i][j] = grid[m - 1][i] + grid[m - 1][j];
+12            }
+13        }
+14        for (int i = m - 2; i >= 0; i--) {
+15            int curr[][] = new int[n][n];
+16            for (int j1 = 0; j1 < n; j1++) {
+17                for (int j2 = 0; j2 < n; j2++) {
+18                    int maxi = (int) -1e8;
+19                    for (int dj1 = -1; dj1 <= 1; dj1++) {
+20                        for (int dj2 = -1; dj2 <= 1; dj2++) {
+21                            int value = 0;
+22                            if (j1 == j2)
+23                                value = grid[i][j1];
+24                            else
+25                                value = grid[i][j1] + grid[i][j2];
+26                            if (j1 + dj1 >= 0 && j1 + dj1 < n && j2 + dj2 >= 0 && j2 + dj2 < n)
+27                                value += prev[j1 + dj1][j2 + dj2];
+28                            else {
+29                                value += (int) -1e8;
+30                            }
+31                            maxi = Math.max(value, maxi);
+32                        }
+33                    }
+34                    curr[j1][j2] = maxi;
+35                }
+36            }
+37            prev = curr;
 38        }
-39        return solve(0, 0, n - 1, m - 1, n - 1, grid, dp);
+39        return prev[0][n - 1];
 40    }
 41}
