ee7650cf8ada2d3ebfe44728eb212d24b8fe7e6b
Shivam Gupta <kapilmohangupta20@gmail.com>
2026-03-30 15:01:48 +0530

    [746] [Time Beats: 38.22%] [Memory Beats: 50.20%] - LeetPush

--- Min-Cost-Climbing-Stairs.java
+++ Min-Cost-Climbing-Stairs.java
@@ -1,15 +1,17 @@
 1class Solution {
-2    // Optimized Tabulation
-3    public int minCostClimbingStairs(int[] cost) {
-4        int n = cost.length;
-5        int val1 = cost[0];
-6        int val2 = cost[1];
-7        for(int i=2; i<cost.length; i++)
-8        {
-9            int temp = cost[i] + Math.min(val1,val2);
-10            val1 = val2;
-11            val2 = temp;
-12        }
-13        return Math.min(val1,val2);
-14    }
-15}
+2    public int solve(int[] cost , int n , int[] dp)
+3    {
+4        if(n==0 || n==1) return cost[n];
+5        if(n<0) return 0;
+6        if(dp[n]!=-1) return dp[n];
+7        int step1 = cost[n] + solve(cost,n-1,dp);
+8        int step2 = cost[n] + solve(cost,n-2,dp);
+9        return dp[n] = Math.min(step1,step2);
+10    }
+11    public int minCostClimbingStairs(int[] cost) {
+12        int N = cost.length-1;
+13        int dp[] = new int[N+2];
+14        Arrays.fill(dp,-1);
+15        return Math.min(solve(cost,N,dp),solve(cost,N-1,dp));
+16    }
+17}
